5676b60fb1a64fe70c0fb5d4bffd155e00cfcb74
Himanshu Joshi <36814920+himanshu07070@users.noreply.github.com>
2024-01-19 00:08:43 +0530

    Update utils.go

--- utils.go
+++ utils.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 )
 
-// createConnection creates a connection to mysql database
-func createConnection() *sql.DB {
+//Modified createConnection to return an error, allowing better error handling and removed the print statement for the SQL open error, as it's already being returned.
+func createConnection() (*sql.DB, error) {
 	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/test")
-	fmt.Println("sql open " + err.Error())
-	return db
+	if err != nil {
+		fmt.Println("Error opening database connection:", err)
+		return nil, err
+	}
+	return db, nil
 }
